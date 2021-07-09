b2715be5acced5c6c797718c621b477f75350a16
Jorge Zúñiga Sandí <jz60023@hotmail.com>
2021-07-08 21:45:57 -0600

    Theme update, "Usuario" added

--- Entities-POJO/UserProfile.cs
+++ Entities-POJO/UserProfile.cs
@@ -10,9 +10,7 @@ namespace Entities_POJO
     {
         public string UserId { get; set; }
         public string UserName { get; set; }
-
         public string FullName { get; set; }
-
         public string Password { get; set; }
 
     }
---
+++ Entities-POJO/Usuario.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_POJO
+{
+    public class Usuario : BaseEntity
+    {
+        public string Id { get; set; }
+        public string Tipo_Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Fecha_Nac { get; set; }
+        public string Contrasenna { get; set; }
+        public string Email { get; set; }
+        public string Estado { get; set; }
+        public string Telefono { get; set; }
+        public string Cod_Email { get; set; }
+        public string Cod_Celular { get; set; }
+        public string Id_Suscripcion { get; set; }
+        public string Id_Rol { get; set; }
+        public string Id_Agencia { get; set; }
+        public char Verificado { get; set; }
+
+        public Usuario() { }
+
+        public Usuario(string[] info) {
+            Id = info[0];
+            Tipo_Id = info[1];
+            Nombre = info[2];
+            Apellidos = info[3];
+            Fecha_Nac = info[4];
+            Contrasenna = info[5];
+            Email = info[6];
+            Estado = info[7];
+            Telefono = info[8];
+            Cod_Email = info[9];
+            Cod_Celular = info[10];
+            Id_Suscripcion = info[11];
+            Id_Rol = info[12];
+            Id_Agencia = info[13];
+            Verificado = Convert.ToChar(info[14]);
+        }
+    }
+}
