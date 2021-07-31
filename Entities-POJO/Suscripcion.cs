69bb1fa10314f8759d562d9ada3dd37bffdff3c5
Stuart Libby <cs_libby@hotmail.com>
2021-07-31 15:39:34 +0000

    Added Suscripcion.cs to /Entities-POJO

---
+++ Entities-POJO/Suscripcion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_POJO
+{
+    public class Suscripcion : BaseEntity
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad_Anuncios { get; set; }
+        public string Periodo_Facturacion { get; set; }
+
+        public Suscripcion() { }
+
+        public Suscripcion(string[] info)
+        {
+            Id = info[0];
+            Nombre = info[1];
+            Cantidad_Anuncios = Convert.ToInt32(info[2]);
+            Periodo_Facturacion = info[3];
+        }
+    }
+
+   
+}
